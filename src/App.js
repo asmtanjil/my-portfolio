4391d760915a9694a0255e8321a39d0e74906215
asmtanjil <asm.tanjil@gmail.com>
2022-12-10 19:22:22 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App max-w-[1440px] mx-auto">
+
+      {/* Navbar */}
+      <nav className='flex justify-between my-4'>
+        <div>
+          <h2><span>Tanjil's</span> PortFolio</h2>
+        </div>
+        <div className='flex gap-8'>
+          <a href="https://drive.google.com/file/d/1roDcdkd4gdSJrbLr9Wn-_I4PJ3d6Vc8E/view?usp=share_link">Resume</a>
+          <a href="/about">About</a>
+          <a href="/projects">Projects</a>
+          <a href="/contact">Contact ME</a>
+        </div>
+      </nav>
+
+      <section className='my-96'>
+        <div>
+          I'm a front‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent website that improves the lives of those around me.
+        </div>
+      </section>
+
     </div>
   );
 }
